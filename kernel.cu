e535a30fd267efbab7130db02a775edfc617e97f
DT2131 <zymdt2131@163.com>
2019-03-08 14:58:08 +0800

    Sigmoid version 0.1

--- kernel.cu
+++ kernel.cu
@@ -41,9 +41,6 @@ typedef struct Layer {
 	void sigmoid() {
 		for (int i = 0; i < neuronNumber; i++) {
 			neuronsValue[i] = 1.0 / (1.0 + exp(-neuronsValue[i]));
-			if (isnan(neuronsValue[i])) {
-				neuronsValue[i] = 0;
-			}
 		}
 	}
 }Layer;
@@ -57,6 +54,7 @@ typedef struct DataSet {
 	vector <int> size;
 	vector <double> data;
 	vector <double> labels;
+	vector <double> labelVectors;
 	unsigned int in(ifstream& icin, unsigned int size) {
 		unsigned int ans = 0;
 		for (int i = 0; i < size; i++) {
@@ -98,11 +96,12 @@ typedef struct DataSet {
 		checker = in(icin, 4), number = in(icin, 4);
 		for (int i = 0; i < number; i++) {
 			int temp = in(icin, 1);
-			for (int j = 0; j < temp; j++) labels.push_back(0);
-			labels.push_back(1);
-			for (int j = temp + 1; j < 10; j++) labels.push_back(0);
+			labels.push_back(temp);
+			for (int j = 0; j < temp; j++) labelVectors.push_back(0);
+			labelVectors.push_back(1);
+			for (int j = temp + 1; j < 10; j++) labelVectors.push_back(0);
 		}
-		singleLabelSize = labels.size() / number;
+		singleLabelSize = labelVectors.size() / number;
 	}
 }DataSet;
 
@@ -150,6 +149,7 @@ typedef struct Model {
 		int simple = 0;
 		for (int i = 0; i < batchNumber; i++) {
 
+			int tempmaxnum;
 			//	initialize weightsBatchadjust and biasBatchadjust
 
 			for (int j = 1; j < layerSize; j++) {
@@ -164,7 +164,7 @@ typedef struct Model {
 
 			//	single batch begin
 
-			double batchCost = 0, batchAccuracy = 0;		//	batch accuracy
+			double batchCost = 0, batchTop1Accuracy = 0, batchTop3Accuracy = 0;		//	batch accuracy
 
 			if (batchshift) simple = (rand() * rand()) % data.number;
 			for (int j = 0; j < batchSize; j++) {
@@ -197,17 +197,7 @@ typedef struct Model {
 
 						for (int u = 0; u < layers[k - 1].neuronNumber; u++) {
 							for (int v = 0; v < layers[k].neuronNumber; v++) {
-
-								//	TEST W 
-								if (isnan(layers[k].weights[u * layers[k].neuronNumber + v]))
-									layers[k].weights[u * layers[k].neuronNumber + v] = 0;
-
 								layers[k].neuronsValue[v] = layers[k - 1].neuronsValue[u] * layers[k].weights[u * layers[k].neuronNumber + v];
-
-								//	TEST BA 
-								if (isnan(layers[k].neuronsValue[v])) {
-									layers[k].neuronsValue[v] = 0;
-								}
 							}
 						}
 
@@ -215,12 +205,6 @@ typedef struct Model {
 
 						for (int v = 0; v < layers[k].neuronNumber; v++) {
 							layers[k].neuronsValue[v] += layers[k].bias[v];
-
-							//	TEST B
-
-							if (isnan(layers[k].neuronsValue[v])) {
-								layers[k].neuronsValue[v] = 0;
-							}
 						}
 						// activate
 
@@ -247,7 +231,7 @@ typedef struct Model {
 
 								for (int v = 0; v < layers[k].neuronNumber; v++) {
 									if (k == layerSize - 1) {
-										layers[k].neuronsadjust[v] = 2 * (layers[k].neuronsValue[v] - data.labels[simple * data.singleLabelSize + v]);
+										layers[k].neuronsadjust[v] = 2 * (layers[k].neuronsValue[v] - data.labelVectors[simple * data.singleLabelSize + v]);
 									}
 									else {
 										layers[k].neuronsadjust[v] = 0;
@@ -256,26 +240,8 @@ typedef struct Model {
 										}
 									}
 									for (int u = 0; u < layers[k - 1].neuronNumber; u++) {
-										if (isnan(layers[k].neuronsadjust[v])) {
-											layers[k].neuronsadjust[v] = 0;
-										}
-										if (isnan(layers[k].neuronsValue[v])) {
-											layers[k].neuronsValue[v] = 0;
-										}
-										if (isnan(layers[k - 1].neuronsValue[u])) {
-											layers[k - 1].neuronsValue[u] = 0;
-										}
 										layers[k].weightsSingleAdjust[u * layers[k].neuronNumber + v] = layers[k].neuronsadjust[v] * layers[k].neuronsValue[v] * (1.0 - layers[k].neuronsValue[v]) * layers[k - 1].neuronsValue[u];
-										if (isnan(layers[k].weightsSingleAdjust[u * layers[k].neuronNumber + v])) {
-											layers[k].weightsSingleAdjust[u * layers[k].neuronNumber + v] = 0;
-										}
-										if (isinf(layers[k].weightsSingleAdjust[u * layers[k].neuronNumber + v])) {
-											layers[k].weightsSingleAdjust[u * layers[k].neuronNumber + v] = 0;
-										}
 										layers[k].weightsBatchAdjust[u * layers[k].neuronNumber + v] += layers[k].weightsSingleAdjust[u * layers[k].neuronNumber + v];
-										if (isnan(layers[k].weightsBatchAdjust[u * layers[k].neuronNumber + v])) {
-											layers[k].weightsBatchAdjust[u * layers[k].neuronNumber + v] = 0;
-										}
 									}
 								}
 
@@ -283,11 +249,6 @@ typedef struct Model {
 
 								for (int v = 0; v < layers[k].neuronNumber; v++) {
 									layers[k].biasSingleAdjust[v] = layers[k].neuronsadjust[v] * layers[k].neuronsValue[v] * (1 - layers[k].neuronsValue[v]);
-									if (isnan(layers[k].biasSingleAdjust[v])) {
-										layers[k].biasSingleAdjust[v] = 0;
-									}
-									if (fabs(layers[k].biasSingleAdjust[v]) > 1000)
-										layers[k].biasSingleAdjust[v] = layers[k].biasSingleAdjust[v];
 									layers[k].biasBatchAdjust[v] += layers[k].biasSingleAdjust[v];
 								}
 							}
@@ -306,15 +267,38 @@ typedef struct Model {
 				
 				//	accuracy count
 				double tempmax = -1;
-				int tempmaxnum = -1;
+
+				vector< pair <double, int> > output;
+
+				//	calculate cost
+
 				for (int k = 0; k < layers[layerSize - 1].neuronNumber; k++) {
-					batchCost += layers[layerSize - 1].neuronsadjust[k];
-					if (layers[layerSize - 1].neuronsValue[k] > tempmax) {
-						tempmax = layers[layerSize - 1].neuronsValue[k];
-						tempmaxnum = k;
+					batchCost += (layers[layerSize - 1].neuronsValue[k] - data.labelVectors[simple * data.singleLabelSize + k]) * (layers[layerSize - 1].neuronsValue[k] - data.labelVectors[simple * data.singleLabelSize + k]);
+				}
+
+				//	calculate accuracy
+
+				for (int k = 0; k < layers[layerSize - 1].neuronNumber; k++) {
+					output.push_back({ layers[layerSize - 1].neuronsValue[k],k });
+				}
+				sort(output.begin(), output.end());
+				if (output[output.size() - 1].second == data.labels[simple]) batchTop1Accuracy++;
+				for (int k = output.size() - 1; k >= output.size() - 3; k--) {
+					if (output[k].second == data.labels[simple]) {
+						batchTop3Accuracy++; 
+						break;
+
 					}
 				}
-				if (tempmaxnum != -1 && data.labels[(simple * data.singleLabelSize + tempmaxnum) % data.number] == 1) batchAccuracy++;
+				
+				//for (int k = 0; k < layers[layerSize - 1].neuronNumber; k++) {
+				//	batchCost= layers[layerSize-1].neuronsValue[k] - data.labels[simple * data.singleLabelSize + k];
+				//	if (layers[layerSize - 1].neuronsValue[k] > tempmax) {
+				//		tempmax = layers[layerSize - 1].neuronsValue[k];
+				//		tempmaxnum = k;
+				//	}
+				//}
+				
 				
 				//	go to next simple
 				
@@ -329,28 +313,14 @@ typedef struct Model {
 			for (int k = 1; k < layerSize; k++) {
 				for (int u = 0; u < layers[k - 1].neuronNumber; u++) {
 					for (int v = 0; v < layers[k].neuronNumber; v++) {
-						if (isnan(layers[k].weightsBatchAdjust[u * layers[k].neuronNumber + v])) {
-							layers[k].weightsBatchAdjust[u * layers[k].neuronNumber + v] = 0;
-						}
 						layers[k].weightsBatchAdjust[u * layers[k].neuronNumber + v] /= batchSize;
-						if (isnan(layers[k].weightsBatchAdjust[u * layers[k].neuronNumber + v])) {
-							layers[k].weightsBatchAdjust[u * layers[k].neuronNumber + v] = 0;
-						}
 						layers[k].weightsBatchAdjust[u * layers[k].neuronNumber + v] *= layers[k].learningRate;
-						if (isnan(layers[k].weightsBatchAdjust[u * layers[k].neuronNumber + v])) {
-							layers[k].weightsBatchAdjust[u * layers[k].neuronNumber + v] = 0;
-						}
 						layers[k].weights[u * layers[k].neuronNumber + v] += layers[k].weightsBatchAdjust[u*layers[k].neuronNumber + v];
 					}
 				}
 				for (int u = 0; u < layers[k].neuronNumber; u++) {
 					layers[k].biasBatchAdjust[u] /= batchSize;
 					layers[k].biasBatchAdjust[u] *= layers[k].learningRate;
-					if (isnan(layers[k].biasBatchAdjust[u])) {
-						layers[k].biasBatchAdjust[u] = 0;
-					}
-					if (fabs(layers[k].biasBatchAdjust[u]) > 1000)
-						layers[k].biasBatchAdjust[u] = layers[k].biasBatchAdjust[u];
 					layers[k].bias[u] += layers[k].biasBatchAdjust[u];
 				}
 			}
@@ -359,8 +329,11 @@ typedef struct Model {
 			//if (batchAccuracy == 0) {
 			//	batchAccuracy = 0;
 			//}
-			cout << "Batch No. " << i << " Simple No. " << batchSize * i << " Cost: " << batchCost / batchSize << " Accuracy: " << batchAccuracy / batchSize << endl;
-		}
+			cout << "Batch No. " << i << " Simple No. " << batchSize * i << " Cost: " << batchCost / batchSize << " Top 1: " << batchTop1Accuracy / batchSize << " Top 3: " << batchTop3Accuracy / batchSize <<endl;
+			if (i % 20 == 0) {
+				i = i;
+			}
+}
 	}
 }Model;
 
@@ -376,7 +349,13 @@ void buildModel(Model& model) {
 		});
 	model.layers.push_back({
 		"fullConnect",
-		25,
+		24,
+		"sigmoid",
+		-0.5
+		});
+	model.layers.push_back({
+		"fullConnect",
+		24,
 		"sigmoid",
 		-0.5
 		});
@@ -393,5 +372,6 @@ int main()
 	mnist.MNISTinput();
 	//mnist.MNISTInputDebug();
 	buildModel(myModel);
-	myModel.train(100000, 10, 0, mnist);
+	myModel.train(100000, 100, 1, mnist);
+	cout << endl;
 }
